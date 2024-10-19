b73cf45d0d6030a96972a73c9aba4f0b82061dd9
Ahmed Alamassi <christianalamassi@gmail.com>
2024-10-19 15:27:32 +0000

    Create table

--- myduties/src/Main.js
+++ myduties/src/Main.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 
-function Main() {
+const Main = () => {
+
     return (
-        <div>Main</div>
+        <div>
+            <form></form>
+            <h1>The tasks of today</h1>
+            <div className='box'> </div>
+            <table>
+                <tr>
+                    <th>Home Work</th>
+                    <th>Time</th>
+                    <th>Done</th>
+                </tr>
+                <tr>
+                    <td><input></input></td>
+                    <td> <label for="time1"></label>
+                    <input type="time" id="time1" name="time1"></input></td>
+                    <td>
+                    <label for="checkbox1"></label>
+                    <input type="checkbox" id="checkbox1" name="checkbox1" value="Bike"></input></td>
+                </tr>
+                <tr>
+                <td><input></input></td>
+                    <td> <label for="time1"></label>
+                    <input type="time" id="time1" name="time1"></input></td>
+                    <td>
+                    <label for="checkbox1"></label>
+                    <input type="checkbox" id="checkbox1" name="checkbox1" value="Bike"></input></td>
+                </tr>
+                <tr>
+                <td><input></input></td>
+                    <td> <label for="time1"></label>
+                    <input type="time" id="time1" name="time1"></input></td>
+                    <td>
+                    <label for="checkbox1"></label>
+                    <input type="checkbox" id="checkbox1" name="checkbox1" value="Bike"></input></td>
+                </tr>
+                <tr>
+                <td><input></input></td>
+                    <td> <label for="time1"></label>
+                    <input type="time" id="time1" name="time1"></input></td>
+                    <td>
+                    <label for="checkbox1"></label>
+                    <input type="checkbox" id="checkbox1" name="checkbox1" value="Bike"></input></td>
+                </tr>
+                <tr>
+                <td><input></input></td>
+                    <td> <label for="time1"></label>
+                    <input type="time" id="time1" name="time1"></input></td>
+                    <td>
+                    <label for="checkbox1"></label>
+                    <input type="checkbox" id="checkbox1" name="checkbox1" value="Bike"></input></td>
+                </tr>
+                <tr>
+                <td><input></input></td>
+                    <td> <label for="time1"></label>
+                    <input type="time" id="time1" name="time1"></input></td>
+                    <td>
+                    <label for="checkbox1"></label>
+                    <input type="checkbox" id="checkbox1" name="checkbox1" value="Bike"></input></td>
+                </tr>
+                <tr>
+                <td><input></input></td>
+                    <td> <label for="time1"></label>
+                    <input type="time" id="time1" name="time1"></input></td>
+                    <td>
+                    <label for="checkbox1"></label>
+                    <input type="checkbox" id="checkbox1" name="checkbox1" value="Bike"></input></td>
+                </tr>
+            </table>
+        </div>
     )
 }
 
